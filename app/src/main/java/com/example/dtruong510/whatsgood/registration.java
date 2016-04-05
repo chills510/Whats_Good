8d39aa557105f7215b2d1e9ef414a1aed75d3a93
chueyee <chueyee03@hotmail.com>
2016-04-04 22:41:29 -0700

    Need to finish homepage in order to link profile, create services etc. Need to link DB
    **UPDATE**
    Tried creating DB through Android Studio's library but couldnt do it so i erased it but the whole password confirm match crap works so yeah. if it doesnt match youll be informed of the event.

--- app/src/main/java/com/example/dtruong510/whatsgood/registration.java
+++ app/src/main/java/com/example/dtruong510/whatsgood/registration.java
@@ -1,16 +1,18 @@
 package com.example.dtruong510.whatsgood;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class registration extends AppCompatActivity {
 
     private Button cancelBtn, registerBtn;
+    private EditText passwordET, confirmET, emailET;
+    private String email, password, confirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +35,28 @@ public class registration extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
-                //need to link up to DB and add username and password
-                finish();
+                passwordET = (EditText) findViewById(R.id.etPassword);
+                password = passwordET.getText().toString();
+
+                confirmET = (EditText) findViewById(R.id.etConfirmPass);
+                confirm = confirmET.getText().toString();
+
+                if(password.equals(confirm))
+                {
+                        finish();
+                }
+                else
+                {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Passwords Do Not Match!";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+
             }
         });
-
     }
 
 }
--- app/src/main/java/com/example/dtruong510/whatsgood/start_up.java
+++ app/src/main/java/com/example/dtruong510/whatsgood/start_up.java
@@ -2,15 +2,10 @@ package com.example.dtruong510.whatsgood;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.EditText;
+
 
 public class start_up extends AppCompatActivity {
 
@@ -20,6 +15,7 @@ public class start_up extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_up);
+
 
         lButton = (Button) findViewById(R.id.loginButton);
         lButton.setOnClickListener(new View.OnClickListener()
