d3cdbac7ed270448500e0f13bd29ccb20292a08f
chills510 <dtruong510@gmail.com>
2016-04-11 01:16:32 -0700

    added navigation

--- app/src/main/java/com/example/dtruong510/whatsgood/homepage.java
+++ app/src/main/java/com/example/dtruong510/whatsgood/homepage.java
@@ -9,8 +9,17 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+
 
 public class homepage extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
