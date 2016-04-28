b7c7f0035732df1bbacf9ffd65eef9d84c96a89f
noorahmad <noorahmad23@hotmail.com>
2016-04-28 14:37:41 -0700

    Changed UI background and text color, changed font, currently working on changing the buttons and their interactions

--- app/src/main/java/com/example/dtruong510/whatsgood/my_services.java
+++ app/src/main/java/com/example/dtruong510/whatsgood/my_services.java
@@ -5,7 +5,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class my_services extends AppCompatActivity {
 
